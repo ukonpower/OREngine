8c10223825b160d37359d749d4a9642c723d9ad9
ukonpower <ore.ukonpower@gmail.com>
2024-01-19 09:03:33 +0900

    wip

--- src/ts/pages/Editor/index.tsx
+++ src/ts/pages/Editor/index.tsx
@@ -1,5 +1,7 @@
 import style from './index.module.scss';
 
+import { Controls } from '~/ts/components/Controls';
+import { PropertyEditor } from '~/ts/components/PropertyEditor';
 import { GLCanvas } from '~/ts/gl/GLCanvas';
 import { useGL, GLContext } from '~/ts/gl/useGL';
 
@@ -9,7 +11,19 @@ export const EditorPage = () => {
 
 	return <GLContext.Provider value={glContext}>
 		<div className={style.editor}>
-			<GLCanvas />
+			<div className={style.vert}>
+				<div className={style.horiz}>
+					<div className={style.preview}>
+						<GLCanvas />
+					</div>
+					<div className={style.property}>
+						<PropertyEditor />
+					</div>
+				</div>
+				<div className={style.controls}>
+					<Controls />
+				</div>
+			</div>
 		</div>
 	</GLContext.Provider>;
 
