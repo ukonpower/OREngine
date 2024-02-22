e5caddfe35b87e4a4c7f37a2d54bfbb1127b207e
ukonpower <ore.ukonpower@gmail.com>
2024-02-23 02:28:39 +0900

    component categorize

--- src/ts/gl/Scene/Resources/index.ts
+++ src/ts/gl/Scene/Resources/index.ts
@@ -2,74 +2,22 @@
 import * as GLP from 'glpower';
 import * as MXP from 'maxpower';
 
-import { BLidgeClient } from '../../Scene/Resources/Components/BLidgeClient';
-import { LookAt } from '../../Scene/Resources/Components/LookAt';
-import { ShakeViewer } from '../../Scene/Resources/Components/ShakeViewer';
-
 type ComponentArgs = {[key: string]: any}
 
-type ComponentItem = {
-	name: string;
+export type ResouceComponentItem = {
+	key: string;
 	component: typeof MXP.Component;
 	defaultArgs?: ComponentArgs
 };
 
 export class OREngineResource extends GLP.EventEmitter {
 
-	public componentList: ( ComponentItem )[] = [];
+	public componentListCategrized: Map<string, ( ResouceComponentItem )[]> = new Map();
+	public componentList: ( ResouceComponentItem )[] = [];
 
 	constructor() {
 
 		super();
-
-		// object
-
-		this.componentList.push( { name: "camera", component: MXP.Camera, defaultArgs: {
-			cameraType: "perspective",
-		} } );
-
-		this.componentList.push( { name: "light", component: MXP.Light, defaultArgs: {
-			lightType: "directional",
-		} } );
-
-		// geometry
-
-		this.componentList.push( { name: "geometry", component: MXP.SphereGeometry, defaultArgs: {
-			radius: 0.5,
-			widthSegments: 20,
-			heightSegments: 10,
-		} } );
-
-		this.componentList.push( { name: "geometry", component: MXP.CubeGeometry, defaultArgs: {
-			width: 1,
-			height: 1,
-			depth: 1,
-		} } );
-
-		this.componentList.push( { name: "geometry", component: MXP.CylinderGeometry, defaultArgs: {
-			radiusTop: 0.5,
-			radiusBottom: 0.5,
-			height: 1,
-			radSegments: 10,
-			heightSegments: 1
-		} } );
-
-		// material
-
-		this.componentList.push( { name: "material", component: MXP.Material } );
-
-		// camera
-
-		this.componentList.push( { name: "lookAt", component: LookAt } );
-
-		this.componentList.push( { name: "shakeViewer", component: ShakeViewer, defaultArgs: {
-			power: 1.0,
-			speed: 1.0
-		} } );
-
-		// entity
-
-		this.componentList.push( { name: "blidgeClient", component: BLidgeClient } );
 
 	}
 
@@ -83,9 +31,33 @@ export class OREngineResource extends GLP.EventEmitter {
 
 	}
 
-	public registerComponent( component: ComponentItem ) {
+	public clearComponents() {
 
-		this.componentList.push( component );
+		this.componentList.length = 0;
+
+	}
+
+	public componentCategory( catName: string ) {
+
+		const catCompList = this.componentListCategrized.get( catName ) || [];
+
+		this.componentListCategrized.set( catName, catCompList );
+
+		return {
+			register: ( key: string, component: typeof MXP.Component, defaultArgs?: ComponentArgs ) => {
+
+				const compItem = {
+					key,
+					component,
+					defaultArgs
+				};
+
+				this.componentList.push( compItem );
+
+				catCompList.push( compItem );
+
+			}
+		};
 
 	}
 
--- src/ts/gl/Scene/Resources/init.ts
+++ src/ts/gl/Scene/Resources/init.ts
@@ -1,14 +1,90 @@
+import * as MXP from 'maxpower';
+
 import { SkyBox } from "../../Scene/Resources/Components/SkyBox";
 
+import { BLidgeClient } from "./Components/BLidgeClient";
+import { LookAt } from "./Components/LookAt";
 import { OREngineLogo } from "./Components/OREngineLogo";
-import { OREnginePrimitiveSelector } from "./Components/OREnginePrimitiveSelector";
+import { ShakeViewer } from "./Components/ShakeViewer";
+import { TurnTable } from './Components/TurnTable';
 
 import { resource } from "~/ts/Globals";
 
 export const initResouces = () => {
 
-	resource.registerComponent( { name: "material", component: OREngineLogo } );
-	resource.registerComponent( { name: "skybox", component: SkyBox } );
-	resource.registerComponent( { name: "primitiveSelector", component: OREnginePrimitiveSelector } );
+	/*-------------------------------
+		Components
+	-------------------------------*/
+
+	resource.clearComponents();
+
+	// object
+
+	const comObject = resource.componentCategory( "Object" );
+
+	comObject.register( "camera", MXP.Camera, {
+		cameraType: "perspective",
+	} );
+
+	comObject.register( "light", MXP.Light, {
+		lightType: "directional",
+	} );
+
+	// geometry
+
+	const comGeometry = resource.componentCategory( "Geometry" );
+
+	comGeometry.register( "geometry", MXP.SphereGeometry, {
+		radius: 0.5,
+		widthSegments: 20,
+		heightSegments: 10,
+	} );
+
+	comGeometry.register( "geometry", MXP.CubeGeometry, {
+		width: 1,
+		height: 1,
+		depth: 1,
+	} );
+
+	comGeometry.register( "geometry", MXP.CylinderGeometry, {
+		radiusTop: 0.5,
+		radiusBottom: 0.5,
+		height: 1,
+		radSegments: 10,
+		heightSegments: 1
+	} );
+
+	// material
+
+	const comMaterial = resource.componentCategory( "Material" );
+
+	comMaterial.register( "material", MXP.Material );
+
+	// view
+
+	const comView = resource.componentCategory( "Controls" );
+
+	comView.register( "lookAt", LookAt );
+
+	comView.register( "shakeViewer", ShakeViewer, {
+		power: 1.0,
+		speed: 1.0
+	} );
+
+	comView.register( "turntable", TurnTable );
+
+	// entity
+
+	const comEntity = resource.componentCategory( "Entity" );
+
+	comEntity.register( "orengine", OREngineLogo );
+	comEntity.register( "skybox", SkyBox );
+
+	// Other
+
+	const comOther = resource.componentCategory( "Other" );
+
+	comOther.register( "blidgeClient", BLidgeClient );
+
 
 };
