3670ca7d520948355c5d1851e806b7f09e5d5d92
ukonpower <ore.ukonpower@gmail.com>
2024-02-25 09:52:38 +0900

    scene

--- src/ts/gl/Scene/Resources/Components/SkyBox/shaders/skybox.fs
+++ src/ts/gl/Scene/Resources/Components/SkyBox/shaders/skybox.fs
@@ -2,6 +2,7 @@
 #include <packing>
 #include <frag_h>
 #include <noise>
+#include <rotate>
 
 uniform float uTime;
 
@@ -18,13 +19,24 @@ void main( void ) {
 	vec3 normal = normalize( - vNormal );
 	outRoughness = 1.0;
 	outColor *= 0.0;
-	outEmission = vec3( 0.0, 0.01, 0.1);
+	outEmission = vec3( 0.0, 0.05, 0.1);
 
 	float n = noise( outPos * 0.05 + uTime * 0.1 );
-	float n2 = noise( outPos * 0.04 + vec3( 0.0, 0.0, uTime * 0.2 + n ) );
 
-	outEmission += smoothstep( 0.7, 1.0, fract( n2 * 4.5 ) ) * random( vPos.xy ) * 0.5;
-	outEmissionIntensity = 1.0;
+	vec3 n2Pos = outPos;
+	n2Pos.xz *= rotate( n2Pos.y * 0.02 );
+	float n2 = noise( n2Pos * 0.04 + vec3( 0.0, 0.0, uTime * 0.1 + n ) );
+
+	float phase = 4.5;
+
+	float line = smoothstep( 0.88, 0.9, fract( n2 * phase ) );
+	float pattern = smoothstep( 0.2, 0.1, length( fract( ( vUv + vec2( floor(vUv.y * 150.0) / 150.0 * 0.25, 0.0 ) ) * vec2( 2.0, 1.0 ) * 150.0 ) - 0.5 )) * step( n2 * phase, 2.0 ) * 0.8;
+
+	float emit = min( line + pattern, 1.0 );
+
+	outEmission += emit;
+
+	outEmissionIntensity = 1.0 + emit * 50.0 * smoothstep( 0.4, 1.0, n);
 	outEnv = 0.0;
 
 	// discard;
--- src/ts/gl/Scene/Resources/Materials/OREngineLogo/shaders/orengineLogo.fs
+++ src/ts/gl/Scene/Resources/Materials/OREngineLogo/shaders/orengineLogo.fs
@@ -9,13 +9,13 @@ void main( void ) {
 
 	#include <frag_in>
 
-
-	float or = step( vPosBase.x, -0.5 );
+	float or = step( vPosBase.x, -0.2 );
+	float flash = smoothstep(0.3, 0.0,  vNoise) * or;
 	
-	outEmissionIntensity = 3.0;
-	outColor.xyz = vec3( 1.0 );
+	outEmissionIntensity = (1.0 - flash * 0.7) * 3.0;
+	outColor.xyz = vec3( 1.0 - flash );
 	outRoughness = 0.3;
-	outEmission = vec3( 1.0 ) * (1.0 - vNoise);
+	outEmission = vec3( 1.0 );
 	
 	#include <frag_out>
 
