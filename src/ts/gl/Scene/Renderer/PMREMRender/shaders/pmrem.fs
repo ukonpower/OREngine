8244c0125049a257b344a7656ee3d175ba91f29f
ukonpower <ore.ukonpower@gmail.com>
2024-03-12 10:41:48 +0900

    pmrem wip

--- src/ts/gl/Scene/Renderer/PMREMRender/shaders/pmrem.fs
+++ src/ts/gl/Scene/Renderer/PMREMRender/shaders/pmrem.fs
@@ -1,49 +1,97 @@
 #include <common>
 
 uniform sampler2D backbuffer0;
-uniform vec2 uPPPixelSize;
-uniform float uBlur;
-
+uniform sampler2D uPMREMBackBuffer;
+uniform samplerCube uEnvMap;
+uniform float uRenderCount;
+uniform float uRoughness;
+uniform float uTime;
 layout (location = 0) out vec4 outColor;
 
 in vec2 vUv;
 
-#define BLUR_SCALE 4.0
+#include <noise>
 
-vec4 sampleTex( sampler2D tex, vec2 uv, vec2 offset ) {
+// https://www.shadertoy.com/view/4lscWj
 
-	vec2 offsetUV = uv + offset;
+vec2 Hammersley(float i, float numSamples)
+{   
+    uint b = uint(i);
+    
+    b = (b << 16u) | (b >> 16u);
+    b = ((b & 0x55555555u) << 1u) | ((b & 0xAAAAAAAAu) >> 1u);
+    b = ((b & 0x33333333u) << 2u) | ((b & 0xCCCCCCCCu) >> 2u);
+    b = ((b & 0x0F0F0F0Fu) << 4u) | ((b & 0xF0F0F0F0u) >> 4u);
+    b = ((b & 0x00FF00FFu) << 8u) | ((b & 0xFF00FF00u) >> 8u);
+    
+    float radicalInverseVDC = float(b) * 2.3283064365386963e-10;
+    
+    return vec2((i / numSamples), radicalInverseVDC);
+} 
 
-	float originIndexX = floor( uv.x * 3.0 );
-	float originIndexY = floor( uv.y * 2.0 );
+vec3 SampleHemisphere_Cosinus(float i, float numSamples)
+{
+    vec2 xi = Hammersley(i, numSamples);
+    
+    float phi      = xi.y * 2.0 * PI;
+    float cosTheta = sqrt(1.0 - xi.x);
+    float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+     
+    return vec3(cos(phi) * sinTheta, cosTheta, sin(phi) * sinTheta);
+}
 
-	float offsetIndexX = floor( offsetUV.x * 3.0 );
-	float offsetIndexY = floor( offsetUV.y * 2.0 );
+// https://cdn2.unrealengine.com/Resources/files/2013SiggraphPresentationsNotes-26915738.pdf
 
-	return vec4( texture( backbuffer0, offsetUV ).xyz, 1.0 );
+vec3 ImportanceSampleGGX( vec2 Xi, float Roughness, vec3 N ) {
+	float a = Roughness * Roughness;
+	float Phi = 2.0 * PI * Xi.x;
+	float CosTheta = sqrt( (1.0 - Xi.y) / ( 1.0 + (a*a - 1.0) * Xi.y ) );
+	float SinTheta = sqrt( 1.0 - CosTheta * CosTheta );
+	vec3 H;
+	H.x = SinTheta * cos( Phi );
+	H.y = SinTheta * sin( Phi );
+	H.z = CosTheta;
+	vec3 UpVector = abs(N.z) < 0.999 ? vec3(0,0,1) : vec3(1,0,0);
+	vec3 TangentX = normalize( cross( UpVector, N ) );
+	vec3 TangentY = cross( N, TangentX );
+	// Tangent to world space
+	return TangentX * H.x + TangentY * H.y + N * H.z;
+}
+
+vec3 PrefilterEnvMap( float Roughness, vec3 R )
+{
+	vec3 N = R;
+	vec3 V = R;
+	vec3 PrefilteredColor = vec3( 0.0 );
+	float TotalWeight = 0.0;
+	const int NumSamples = 132;
+
+	for( int i = 0; i < NumSamples; i++ ) {
+		
+		vec2 Xi = Hammersley( float(i) + random(vec2(uTime) ) * float(NumSamples), float( NumSamples ) );
+		vec3 H = ImportanceSampleGGX( Xi, Roughness, N );
+		vec3 L = 2.0 * dot( V, H ) * H - V;
+		float NoL = saturate( dot( N, L ) );
+
+		if( NoL > 0.0 ) {
+			PrefilteredColor += texture( uEnvMap , L).rgb * NoL;
+			TotalWeight += NoL;
+		}
+
+	}
 	
+	return PrefilteredColor / TotalWeight;
 }
 
 void main( void ) {
 
 	vec4 sum = vec4( 0.0 );
 
-	float blurSize = uBlur * BLUR_SCALE;
+	vec3 dir = vec3( vUv - 0.5, 1.0 );
+	dir = normalize( dir );
 
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( -uPPPixelSize.x, uPPPixelSize.y ) );
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( 0.0, uPPPixelSize.y ) );
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( uPPPixelSize.x, uPPPixelSize.y ) );
-	
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( -uPPPixelSize.x, 0.0 ) );
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( 0.0, 0.0 ) );
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( uPPPixelSize.x, 0.0 ) );
+	sum.xyz += PrefilterEnvMap(uRoughness, dir);
 
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( -uPPPixelSize.x, -uPPPixelSize.y ) );
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( 0.0, -uPPPixelSize.y ) );
-	sum += sampleTex( backbuffer0, vUv, blurSize * vec2( uPPPixelSize.x, -uPPPixelSize.y ) );
-
-	sum.xyz /= sum.w;
-
-	outColor = vec4( sum.xyz, 1.0 );
+	outColor = vec4( mix( texture( uPMREMBackBuffer, vUv ).xyz, sum.xyz, 0.1 ), 0.0 );
 
 }
