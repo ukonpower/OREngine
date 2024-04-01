2ff04e0f111e7ef797fdb80b09d3bbe20dccc8eb
ukonpower <ore.ukonpower@gmail.com>
2024-04-01 22:28:56 +0900

    trackpad

--- src/ts/components/panel/Timeline/TimelineControls/index.tsx
+++ src/ts/components/panel/Timeline/TimelineControls/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useRef } from 'react';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
 
 import { TimelineContext } from '../hooks/useTimeline';
 
@@ -6,7 +6,7 @@ import style from './index.module.scss';
 
 export const TimelineControls = () => {
 
-	const { viewPort, frame, setFrame, getFrameViewPort, zoom, setViewPortCenter } = useContext( TimelineContext );
+	const { viewPort, frame, setFrame, getFrameViewPort, zoom, scroll, setViewPortCenter } = useContext( TimelineContext );
 
 	const viewPortRef = useRef( [ 0, 0, 0, 0 ] );
 	const viewPortRangeRef = useRef( [ 0, 0 ] );
@@ -94,17 +94,62 @@ export const TimelineControls = () => {
 
 	// wheel
 
-	const onWheel = useCallback( ( e: React.WheelEvent<HTMLDivElement> ) => {
+	const onWheel = useCallback( ( e: WheelEvent ) => {
 
 		if ( pointerDownButtonRef.current !== null ) return;
 
-		zoom && zoom( e.deltaY < 0 ? 0.9 : 1.1 );
+		e.preventDefault();
 
-	}, [ zoom ] );
+		const width = e.target && ( e.target as HTMLElement ).clientWidth || 1;
+
+		const absY = Math.abs( e.deltaY );
+
+		if ( Math.abs( e.deltaX ) < absY ) {
+
+			if ( absY > 50 ) {
+
+				zoom && zoom( e.deltaY < 0 ? 0.9 : 1.1 );
+
+			} else {
+
+				zoom && zoom( 1.0 + e.deltaY * 0.005 );
+
+			}
+
+
+		} else {
+
+			scroll && scroll( e.deltaX / width * 0.5 );
+
+		}
+
+	}, [ zoom, scroll ] );
+
+	useEffect( () => {
+
+		const elm = elmRef.current;
+
+		if ( elm ) {
+
+			elm.addEventListener( "wheel", onWheel, { passive: false } );
+
+		}
+
+		return () => {
+
+			if ( elm ) {
+
+				elm.removeEventListener( "wheel", onWheel );
+
+			}
+
+		};
+
+	}, [ onWheel ] );
 
 	if ( ! viewPort || ! frame ) return null;
 
-	return <div className={style.controls} onPointerDown={onPointerDown} onWheel={onWheel} ref={elmRef}>
+	return <div className={style.controls} onPointerDown={onPointerDown} ref={elmRef}>
 	</div>;
 
 };
