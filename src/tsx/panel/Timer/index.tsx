bd3653997463675eddacfa23d156bda513b27b3e
ukonpower <ore.ukonpower@gmail.com>
2024-10-02 18:26:12 +0900

    wip

--- src/tsx/panel/Timer/index.tsx
+++ src/tsx/panel/Timer/index.tsx
@@ -1,6 +1,16 @@
+import { useContext, useEffect } from 'react';
+
 import style from './index.module.scss';
 
+import { renderer } from '~/ts/gl/GLGlobals';
+import { GLContext } from '~/tsx/gl/useGL';
+
 export const Timer = () => {
+
+	useEffect( () => {
+
+
+	}, [ ] );
 
 	return <div className={style.timer}>
 		aaaa
