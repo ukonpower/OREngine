9714a1658eb2c049ed755f37340029c01c1e7b64
ukonpower <ore.ukonpower@gmail.com>
2024-11-24 21:54:46 +0900

    Lightコンポーネント修正

--- packages/maxpower/Component/Light/index.ts
+++ packages/maxpower/Component/Light/index.ts
@@ -34,13 +34,14 @@ export class Light extends ShadowMapCamera {
 		super( params );
 
 		this.lightType = 'spot';
+		this.cameraType = "perspective";
 
 		this.color = new GLP.Vector( 1.0, 1.0, 1.0, 0.0 );
 		this.intensity = 1;
 
 		// shadow
 
-		this.castShadow = false;
+		this.castShadow = true;
 		this.shadowMapSize = new GLP.Vector( 1024, 1024 );
 
 		// directional
@@ -50,31 +51,10 @@ export class Light extends ShadowMapCamera {
 
 		// spot
 
-		this.angle = 50;
+		this.angle = Math.PI * 0.5;
 		this.blend = 1;
 		this.distance = 30;
 		this.decay = 2;
-
-		this.updateProjectionMatrix();
-
-	}
-
-	protected deserializer( props: TypedSerializableProps<this> ) {
-
-		props = { ...this.serialize(), ...props };
-
-		this.lightType = props.lightType.value;
-
-		if ( this.lightType == 'directional' ) this.cameraType = 'orthographic';
-		if ( this.lightType == 'spot' ) this.cameraType = 'perspective';
-
-		this.color.copy( props.color.value );
-		this.intensity = props.intensity.value;
-		this.angle = props.angle.value;
-		this.blend = props.blend.value;
-		this.distance = props.distance.value;
-		this.decay = props.decay.value;
-		this.castShadow = props.castShadow.value;
 
 		this.updateProjectionMatrix();
 
