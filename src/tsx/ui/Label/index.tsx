3261b242515737ce72d2524730556be7775c2fb2
ukonpower <ore.ukonpower@gmail.com>
2024-11-27 22:13:55 +0900

    entity property viewのスタイルを調整

--- src/tsx/ui/Label/index.tsx
+++ src/tsx/ui/Label/index.tsx
@@ -2,13 +2,15 @@ import style from './index.module.scss';
 
 export const Label: React.FC<{
 	title?: string,
+	vertical?: boolean,
 	children?: React.ReactNode,
+	labelAlign?: "left" | "right"
 }> = ( props ) => {
 
 	return (
-		<div className={style.container}>
-			<div className={style.label}>{props.title}</div>
-			<div className={style.item}>
+		<div className={style.container} data-vertical={props.vertical}>
+			<div className={style.label} style={{ textAlign: props.labelAlign || "left" }} data-vertical={props.vertical}>{props.title}</div>
+			<div className={style.item} data-vertical={props.vertical}>
 				{props.children}
 			</div>
 		</div>
