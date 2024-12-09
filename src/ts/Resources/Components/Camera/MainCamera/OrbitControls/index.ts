cb2cda83a2f4c35a43f7e35c6016f354b3a70715
ukonpower <ore.ukonpower@gmail.com>
2024-12-09 22:57:09 +0900

    Orbitcontrolが動かなかったのはMatrixのアップデートをしていなかったからだよ

--- src/ts/Resources/Components/Camera/MainCamera/OrbitControls/index.ts
+++ src/ts/Resources/Components/Camera/MainCamera/OrbitControls/index.ts
@@ -155,17 +155,16 @@ export class OrbitControls extends MXP.Component {
 		this.lookatMatrix.lookAt( this.eye, this.target, this.up );
 		this.lookatMatrix.decompose( entity.position, entity.quaternion, entity.scale );
 
+		entity.updateMatrix();
+
 		// calc viewmatrix
 
 		const cameraComponent = entity.getComponentByTag<MXP.Camera>( "camera" );
 
 		if ( cameraComponent ) {
 
-			console.log(cameraComponent);
-			
-
 			cameraComponent.viewMatrix.copy( entity.matrixWorld ).inverse();
-			
+
 		}
 
 	}
@@ -205,26 +204,26 @@ export class OrbitControls extends MXP.Component {
 		this.eye.copy( eye );
 		this.target.copy( target );
 
-		
+
 		if ( this.entity ) {
-			
+
 			const parent = this.entity.parent;
-			
+
 			if ( parent ) {
-				
+
 				parent.updateMatrix( true );
-				
+
 				this.target.applyMatrix4( parent.matrixWorld.clone().inverse() );
-				
+
 			}
-			
+
 		}
-		
+
 		this.orbit.x = Math.atan2( this.eye.y - this.target.y, new GLP.Vector( this.eye.x, this.eye.z ).length() - new GLP.Vector( this.target.x, this.target.z ).length() );
 		this.orbit.y = - Math.atan2( this.eye.x - this.target.x, this.eye.z - this.target.z );
-		
+
 		this.distance = this.eye.clone().sub( this.target ).length();
-		
+
 		this.mouseVelOrbit.set( 0, 0, 0 );
 		this.mouseVelMove.set( 0, 0, 0 );
 
