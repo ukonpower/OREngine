0e805b481442b31a0405e73a8abd336e62462b8a
ukonpower <ore.ukonpower@gmail.com>
2025-01-19 19:20:41 +0900

    グリッチとブラーを移植

---
+++ src/ts/Resources/Components/Camera/MainCamera/PostProcess/Finalize/index.ts
@@ -0,0 +1,38 @@
+import * as MXP from 'maxpower';
+
+import finalizeFrag from './shaders/finalize.fs';
+
+import { gl } from '~/ts/Globals';
+
+export class Finalize extends MXP.PostProcess {
+
+	constructor( params: MXP.PostProcessParams ) {
+
+		super( {
+			...params,
+			passes: [
+				new MXP.PostProcessPass( gl, {
+					frag: finalizeFrag,
+				} )
+			]
+		} );
+
+		if ( import.meta.hot ) {
+
+			import.meta.hot.accept( "./shaders/finalize.fs", ( module ) => {
+
+				if ( module ) {
+
+					this.passes[ 0 ].frag = module.default;
+
+				}
+
+				this.passes[ 0 ].requestUpdate();
+
+			} );
+
+		}
+
+	}
+
+}
