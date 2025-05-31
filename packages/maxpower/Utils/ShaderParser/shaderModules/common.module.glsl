2358b6161b5b88fbb5bae0a44aceb3cabb68260c
ukonpower <ore.ukonpower@gmail.com>
2025-05-31 13:59:11 +0900

    普通のsplatはうまくいってそう

--- packages/maxpower/Utils/ShaderParser/shaderModules/common.module.glsl
+++ packages/maxpower/Utils/ShaderParser/shaderModules/common.module.glsl
@@ -72,6 +72,24 @@ vec3 hsv2rgb( vec3 hsv ) {
 	
 }
 
+// color space conversion
+
+vec3 srgbToLinear( vec3 srgb ) {
+	return mix(
+		srgb / 12.92,
+		pow((srgb + 0.055) / 1.055, vec3(2.4)),
+		step(0.04045, srgb)
+	);
+}
+
+vec3 linearToSrgb( vec3 linear ) {
+	return mix(
+		linear * 12.92,
+		pow(linear, vec3(1.0 / 2.4)) * 1.055 - 0.055,
+		step(0.0031308, linear)
+	);
+}
+
 // packing
 
 vec4 floatToRGBA( float v ) {
