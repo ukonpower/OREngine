7c46d1c44ae58520ceb511a7b6080aefc3487e9c
ukonpower <ore.ukonpower@gmail.com>
2025-06-04 18:20:07 +0900

    chore(tests): mock randomUUID

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,10 @@
 (globalThis as any).self = globalThis;
+
+if (!(globalThis as any).crypto) {
+  (globalThis as any).crypto = {
+    randomUUID: () => '00000000-0000-0000-0000-000000000000',
+  } as Crypto;
+} else if (typeof (globalThis as any).crypto.randomUUID !== 'function') {
+  (globalThis as any).crypto.randomUUID = () =>
+    '00000000-0000-0000-0000-000000000000';
+}
