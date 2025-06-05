8155ef5bafd50e5be1eeb90b60f7810c7a22e40b
ukonpower <ore.ukonpower@gmail.com>
2025-06-05 21:11:20 +0900

    Update Storybook config for orengine UI

---
+++ packages/orengine/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from '../tsx/components/Button';
+
+export const Basic = () => <Button>Button</Button>;
+
+export default {
+  title: 'orengine/Button',
+  component: Button,
+};
