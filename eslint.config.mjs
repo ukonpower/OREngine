94596c3ab362036d2990453d4bf8ec79628df4eb
ukonpower <ore.ukonpower@gmail.com>
2025-06-05 21:11:20 +0900

    Update Storybook config for orengine UI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -17,7 +20,7 @@ const compat = new FlatCompat( {
 	allConfig: js.configs.all
 } );
 
-export default [ {
+export default [{
 	ignores: [ "**/dist", "packages/glpower/" ],
 }, ...fixupConfigRules( compat.extends(
 	"mdcs",
@@ -71,4 +74,4 @@ export default [ {
 			},
 		} ],
 	},
-} ];
+}, ...storybook.configs["flat/recommended"]];
