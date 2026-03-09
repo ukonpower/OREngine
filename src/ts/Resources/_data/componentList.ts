5cffcdbb9b5480f4b05a964970254ff5e556d881
ukonpower <ore.ukonpower@gmail.com>
2026-03-09 19:15:52 +0900

    DemoProject以外のprojectsをgitignoreしRaymarchコンポーネントを追加

--- src/ts/Resources/_data/componentList.ts
+++ src/ts/Resources/_data/componentList.ts
@@ -14,6 +14,7 @@ import { ShakeViewer } from '../Components/ObjectControls/CameraShake/index.ts';
 import { LookAt } from '../Components/ObjectControls/LookAt/index.ts';
 import { ObjectRotate } from '../Components/ObjectControls/ObjectRotate/index.ts';
 import { OrbitControls } from '../Components/ObjectControls/OrbitControls/index.ts';
+import { Raymarch } from '../Components/Raymarch/index.ts';
 import { SPZModel } from '../Components/Samples/SPZModel/index.ts';
 import { BLidgeClient } from '../Components/Utilities/BLidgeClient/index.ts';
 import { UniformControls } from '../Components/Utilities/UniformsControls/index.ts';
@@ -44,6 +45,7 @@ export const COMPONENTLIST: {[key: string]: any} = {
 		ObjectRotate,
 		OrbitControls,
 	},
+	Raymarch,
 	Samples: {
 		SPZModel,
 	},
