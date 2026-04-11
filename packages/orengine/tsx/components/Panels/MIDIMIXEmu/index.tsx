ef65b0b0b8be6542c567ea72817c2302f43c3700
ukonpower <ore.ukonpower@gmail.com>
2026-04-11 18:29:08 +0900

    MIDIMIXエミュレータにボタンラベル表示を追加しHTTPS設定をカスタム証明書方式に変更

--- packages/orengine/tsx/components/Panels/MIDIMIXEmu/index.tsx
+++ packages/orengine/tsx/components/Panels/MIDIMIXEmu/index.tsx
@@ -27,13 +27,20 @@ export interface MIDIMIXController {
 	off( event: string, callback: ( ...args: any[] ) => void ): void;
 }
 
-const MIDIButton: React.FC<{id: number, value: number, controller: MIDIMIXController}> = ( props ) => {
+export interface MIDIMIXLabels {
+	buttons?: Record<number, string>;
+}
 
-	return <InputBoolean checked={props.value > 0.5} onChange={( v ) => {
+const MIDIButton: React.FC<{id: number, value: number, controller: MIDIMIXController, label?: string}> = ( props ) => {
 
-		props.controller.emulateControl( 144, props.id, v ? 1.0 : 0.0 );
+	return <div className={style.btnWrap}>
+		<InputBoolean checked={props.value > 0.5} onChange={( v ) => {
 
-	}}/>;
+			props.controller.emulateControl( 144, props.id, v ? 1.0 : 0.0 );
+
+		}}/>
+		{props.label && <span className={style.label}>{props.label}</span>}
+	</div>;
 
 };
 
@@ -47,7 +54,7 @@ const MIDIValue: React.FC<{value: number, id: number, controller: MIDIMIXControl
 
 };
 
-export const MIDIMIXEmu: React.FC<{controller: MIDIMIXController}> = ( { controller } ) => {
+export const MIDIMIXEmu: React.FC<{controller: MIDIMIXController, labels?: MIDIMIXLabels}> = ( { controller, labels } ) => {
 
 	const [ _state, setState ] = React.useState( 0 );
 
@@ -109,70 +116,70 @@ export const MIDIMIXEmu: React.FC<{controller: MIDIMIXController}> = ( { control
 			<MIDIValue id={16} value={controller.getLine( 0 ).values.x} controller={controller}/>
 			<MIDIValue id={17} value={controller.getLine( 0 ).values.y} controller={controller}/>
 			<MIDIValue id={18} value={controller.getLine( 0 ).values.z} controller={controller}/>
-			<MIDIButton id={1} value={controller.getLine( 0 ).btn1} controller={controller}/>
-			<MIDIButton id={2} value={controller.getLine( 0 ).btn2} controller={controller}/>
+			<MIDIButton id={1} value={controller.getLine( 0 ).btn1} controller={controller} label={labels?.buttons?.[ 1 ]}/>
+			<MIDIButton id={2} value={controller.getLine( 0 ).btn2} controller={controller} label={labels?.buttons?.[ 3 ]}/>
 			<MIDIValue id={19} value={controller.getLine( 0 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
 			<MIDIValue id={20} value={controller.getLine( 1 ).values.x} controller={controller}/>
 			<MIDIValue id={21} value={controller.getLine( 1 ).values.y} controller={controller}/>
 			<MIDIValue id={22} value={controller.getLine( 1 ).values.z} controller={controller}/>
-			<MIDIButton id={4} value={controller.getLine( 1 ).btn1} controller={controller}/>
-			<MIDIButton id={5} value={controller.getLine( 1 ).btn2} controller={controller}/>
+			<MIDIButton id={4} value={controller.getLine( 1 ).btn1} controller={controller} label={labels?.buttons?.[ 4 ]}/>
+			<MIDIButton id={5} value={controller.getLine( 1 ).btn2} controller={controller} label={labels?.buttons?.[ 5 ]}/>
 			<MIDIValue id={23} value={controller.getLine( 1 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
 			<MIDIValue id={24} value={controller.getLine( 2 ).values.x} controller={controller}/>
 			<MIDIValue id={25} value={controller.getLine( 2 ).values.y} controller={controller}/>
 			<MIDIValue id={26} value={controller.getLine( 2 ).values.z} controller={controller}/>
-			<MIDIButton id={7} value={controller.getLine( 2 ).btn1} controller={controller}/>
-			<MIDIButton id={8} value={controller.getLine( 2 ).btn2} controller={controller}/>
+			<MIDIButton id={7} value={controller.getLine( 2 ).btn1} controller={controller} label={labels?.buttons?.[ 7 ]}/>
+			<MIDIButton id={8} value={controller.getLine( 2 ).btn2} controller={controller} label={labels?.buttons?.[ 8 ]}/>
 			<MIDIValue id={27} value={controller.getLine( 2 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
 			<MIDIValue id={28} value={controller.getLine( 3 ).values.x} controller={controller}/>
 			<MIDIValue id={29} value={controller.getLine( 3 ).values.y} controller={controller}/>
 			<MIDIValue id={30} value={controller.getLine( 3 ).values.z} controller={controller}/>
-			<MIDIButton id={10} value={controller.getLine( 3 ).btn1} controller={controller}/>
-			<MIDIButton id={11} value={controller.getLine( 3 ).btn2} controller={controller}/>
+			<MIDIButton id={10} value={controller.getLine( 3 ).btn1} controller={controller} label={labels?.buttons?.[ 10 ]}/>
+			<MIDIButton id={11} value={controller.getLine( 3 ).btn2} controller={controller} label={labels?.buttons?.[ 11 ]}/>
 			<MIDIValue id={31} value={controller.getLine( 3 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
 			<MIDIValue id={46} value={controller.getLine( 4 ).values.x} controller={controller}/>
 			<MIDIValue id={47} value={controller.getLine( 4 ).values.y} controller={controller}/>
 			<MIDIValue id={48} value={controller.getLine( 4 ).values.z} controller={controller}/>
-			<MIDIButton id={13} value={controller.getLine( 4 ).btn1} controller={controller}/>
-			<MIDIButton id={14} value={controller.getLine( 4 ).btn2} controller={controller}/>
+			<MIDIButton id={13} value={controller.getLine( 4 ).btn1} controller={controller} label={labels?.buttons?.[ 13 ]}/>
+			<MIDIButton id={14} value={controller.getLine( 4 ).btn2} controller={controller} label={labels?.buttons?.[ 14 ]}/>
 			<MIDIValue id={49} value={controller.getLine( 4 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
 			<MIDIValue id={50} value={controller.getLine( 5 ).values.x} controller={controller}/>
 			<MIDIValue id={51} value={controller.getLine( 5 ).values.y} controller={controller}/>
 			<MIDIValue id={52} value={controller.getLine( 5 ).values.z} controller={controller}/>
-			<MIDIButton id={16} value={controller.getLine( 5 ).btn1} controller={controller}/>
-			<MIDIButton id={17} value={controller.getLine( 5 ).btn2} controller={controller}/>
+			<MIDIButton id={16} value={controller.getLine( 5 ).btn1} controller={controller} label={labels?.buttons?.[ 16 ]}/>
+			<MIDIButton id={17} value={controller.getLine( 5 ).btn2} controller={controller} label={labels?.buttons?.[ 17 ]}/>
 			<MIDIValue id={53} value={controller.getLine( 5 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
 			<MIDIValue id={54} value={controller.getLine( 6 ).values.x} controller={controller}/>
 			<MIDIValue id={55} value={controller.getLine( 6 ).values.y} controller={controller}/>
 			<MIDIValue id={56} value={controller.getLine( 6 ).values.z} controller={controller}/>
-			<MIDIButton id={19} value={controller.getLine( 6 ).btn1} controller={controller}/>
-			<MIDIButton id={20} value={controller.getLine( 6 ).btn2} controller={controller}/>
+			<MIDIButton id={19} value={controller.getLine( 6 ).btn1} controller={controller} label={labels?.buttons?.[ 19 ]}/>
+			<MIDIButton id={20} value={controller.getLine( 6 ).btn2} controller={controller} label={labels?.buttons?.[ 20 ]}/>
 			<MIDIValue id={57} value={controller.getLine( 6 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
 			<MIDIValue id={58} value={controller.getLine( 7 ).values.x} controller={controller}/>
 			<MIDIValue id={59} value={controller.getLine( 7 ).values.y} controller={controller}/>
 			<MIDIValue id={60} value={controller.getLine( 7 ).values.z} controller={controller}/>
-			<MIDIButton id={22} value={controller.getLine( 7 ).btn1} controller={controller}/>
-			<MIDIButton id={23} value={controller.getLine( 7 ).btn2} controller={controller}/>
+			<MIDIButton id={22} value={controller.getLine( 7 ).btn1} controller={controller} label={labels?.buttons?.[ 22 ]}/>
+			<MIDIButton id={23} value={controller.getLine( 7 ).btn2} controller={controller} label={labels?.buttons?.[ 23 ]}/>
 			<MIDIValue id={61} value={controller.getLine( 7 ).values.w} controller={controller}/>
 		</div>
 		<div className={style.row}>
-			<MIDIButton id={25} value={controller.side.btn1} controller={controller}/>
-			<MIDIButton id={26} value={controller.side.btn2} controller={controller}/>
-			<MIDIButton id={27} value={controller.side.btn3} controller={controller}/>
+			<MIDIButton id={25} value={controller.side.btn1} controller={controller} label={labels?.buttons?.[ 25 ]}/>
+			<MIDIButton id={26} value={controller.side.btn2} controller={controller} label={labels?.buttons?.[ 26 ]}/>
+			<MIDIButton id={27} value={controller.side.btn3} controller={controller} label={labels?.buttons?.[ 27 ]}/>
 			<MIDIValue id={62} value={controller.side.master} controller={controller}/>
 		</div>
 	</div>;
