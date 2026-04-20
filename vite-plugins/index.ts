97942043431a314665ed50d02ba05eca626f1399
ukonpower <ore.ukonpower@gmail.com>
2026-04-20 10:13:13 +0900

    plugins→vite-plugins にリネーム・vite-configs.tsに統合: templates/scripts/replace-namesを削除

--- vite-plugins/index.ts
+++
@@ -1,4 +0,0 @@
-export { ResourceManager } from '../plugins/ResourceManager';
-export { ShaderMinifierLoader } from '../plugins/ShaderMinifierLoader';
-export { ViteErrorReporter } from '../plugins/ViteErrorReporter';
-export { MangledJsonLoader, SaveNameCache, nameCache } from '../plugins/MangleManager';
