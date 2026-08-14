370f30043f1f9594eff9849fb98df3bd2f0df548
ukonpower <ukonpower@users.noreply.github.com>
2026-08-14 01:12:32 +0000

    Deploy preview for PR 30 🛫

---
+++ pr-preview/pr-30/storybook/assets/index.stories-Ci6arwtO.js
@@ -0,0 +1,13 @@
+import{i as e,s as t}from"./preload-helper-BdFrVu1K.js";import{n}from"./iframe-iIKVfHB9.js";import{t as r}from"./jsx-runtime-BYwl_lCr.js";import{n as i,t as a}from"./InputCheckBox-Bw6X9yk1.js";var o,s,c,l,u,d,f,p,m;e((()=>{o=t(n(),1),i(),s=r(),c=e=>{let[t,n]=(0,o.useState)(e.checked??!1);return(0,s.jsx)(a,{...e,checked:t,onChange:n})},l={title:`ui/Input/InputCheckBox`,component:a,args:{checked:!0},render:e=>(0,s.jsx)(c,{...e}),decorators:[e=>(0,s.jsx)(`div`,{style:{padding:`10px`},children:(0,s.jsx)(e,{})})]},u={},d={args:{checked:!1}},f={args:{readOnly:!0}},p={args:{disabled:!0}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    checked: false
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    readOnly: true
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...p.parameters?.docs?.source}}},m=[`Checked`,`Unchecked`,`ReadOnly`,`Disabled`]}))();export{u as Checked,p as Disabled,f as ReadOnly,d as Unchecked,m as __namedExportsOrder,l as default};
---
+++ pr-preview/pr-30/storybook/assets/index.stories-DLa9TVH5.js
@@ -0,0 +1,14 @@
+import{i as e}from"./preload-helper-BdFrVu1K.js";import{t}from"./jsx-runtime-BYwl_lCr.js";import{n,t as r}from"./Button-D2oeWY6w.js";var i,a,o,s,c,l;e((()=>{n(),i=t(),a={title:`ui/Button`,component:r,decorators:[e=>(0,i.jsx)(`div`,{style:{width:`200px`,padding:`10px`},children:(0,i.jsx)(e,{})})]},o={args:{children:`Save`}},s={args:{children:`Save the current scene to scene.json`}},c={args:{children:`Submit`,type:`submit`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Save'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Save the current scene to scene.json'
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Submit',
+    type: 'submit'
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`,`LongLabel`,`Submit`]}))();export{o as Default,s as LongLabel,c as Submit,l as __namedExportsOrder,a as default};
