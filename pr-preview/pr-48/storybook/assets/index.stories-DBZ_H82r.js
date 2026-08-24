7fd7b7efd38a55305b9ec1cd0ff5ee79b71bb78a
ukonpower <ukonpower@users.noreply.github.com>
2026-08-24 07:09:23 +0000

    Deploy preview for PR 48 🛫

---
+++ pr-preview/pr-48/storybook/assets/index.stories-DBZ_H82r.js
@@ -0,0 +1,23 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-DeHZSEgm.js";import{a as n,i as r}from"./react-BNBcnZXp.js";import{a as i,l as a,n as o,s,t as c}from"./scene-CSlotCr6.js";var l,u,d,f,p,m;function h(){return(h=e((()=>{s(),c(),n(),l=t(),u={title:`OREditor/Layout`,component:r},d=e=>({args:{editorData:o,customTabs:e},decorators:[e=>(0,l.jsx)(`div`,{style:{width:`100vw`,height:`100vh`},children:(0,l.jsx)(e,{})}),a(i)]}),f=d(),p=d({leftTop:[{title:`Assets`,content:(0,l.jsx)(`div`,{children:`custom leftTop`})}],mainBottom:[{title:`Console`,content:(0,l.jsx)(`div`,{children:`custom mainBottom 1`}),default:!0},{title:`Log`,content:(0,l.jsx)(`div`,{children:`custom mainBottom 2`})}],rightTop:[{title:`Custom`,content:(0,l.jsx)(`div`,{children:`custom rightTop`}),default:!0}],footer:[{title:`Notes`,content:(0,l.jsx)(`div`,{children:`custom footer`})}]}),f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`fullscreen()`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`fullscreen({
+  leftTop: [{
+    title: 'Assets',
+    content: <div>custom leftTop</div>
+  }],
+  mainBottom: [{
+    title: 'Console',
+    content: <div>custom mainBottom 1</div>,
+    default: true
+  }, {
+    title: 'Log',
+    content: <div>custom mainBottom 2</div>
+  }],
+  rightTop: [{
+    title: 'Custom',
+    content: <div>custom rightTop</div>,
+    default: true
+  }],
+  footer: [{
+    title: 'Notes',
+    content: <div>custom footer</div>
+  }]
+})`,...p.parameters?.docs?.source}}},m=[`Default`,`CustomTabs`]})))()}h();export{p as CustomTabs,f as Default,m as __namedExportsOrder,u as default};
