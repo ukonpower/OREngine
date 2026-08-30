3421d37fab225c3ba7cd0f8a3c3cf28641244ccc
ukonpower <ukonpower@users.noreply.github.com>
2026-08-30 14:39:55 +0000

    Deploy preview for PR 49 🛫

--- pr-preview/pr-49/storybook/assets/iframe-BHb1sSAQ.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-DaJh8qSi.js";e();
---
+++ pr-preview/pr-49/storybook/assets/index.stories-DKUNt3n_.js
@@ -0,0 +1,14 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{r as t}from"./iframe-DrqfYnLf.js";import{t as n}from"./jsx-runtime-DeHZSEgm.js";import{Xt as r,Yt as i}from"./react-DhWNY0KL.js";import{n as a,t as o}from"./withInputWindow-Dkb1GFxx.js";var s,c,l,u,d,f,p,m,h,g;function _(){return(_=e((()=>{s=t(),o(),r(),c=n(),l=(e,t)=>{let n=e;return Array.from({length:t},(e,t)=>n[t])},u=e=>{let[t,n]=(0,s.useState)(e.value);return(0,c.jsx)(i,{...e,value:t,onChange:e=>n(l(e,t.length))})},d={title:`ui/Vector`,component:i,args:{value:[0,1.5,-2]},render:e=>(0,c.jsx)(u,{...e}),decorators:[e=>(0,c.jsx)(`div`,{style:{width:`200px`,padding:`10px`},children:(0,c.jsx)(e,{})}),a]},f={},p={args:{value:[0,0,0,1]}},m={args:{value:[1920,1080],step:10}},h={args:{disabled:!0}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: [0, 0, 0, 1]
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: [1920, 1080],
+    step: 10
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...h.parameters?.docs?.source}}},g=[`Vec3`,`Vec4`,`Vec2`,`Disabled`]})))()}_();export{h as Disabled,m as Vec2,f as Vec3,p as Vec4,g as __namedExportsOrder,d as default};
