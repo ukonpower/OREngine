7be2955b12ac43e0832a87a2d0d787d6f529982a
ukonpower <ukonpower@users.noreply.github.com>
2026-09-15 09:25:43 +0000

    Deploy preview for PR 54 🛫

---
+++ pr-preview/pr-54/storybook/assets/iframe-D_3D1h1U.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-8KgB8swm.js";e();
---
+++ pr-preview/pr-54/storybook/assets/index.stories-FJMQryGb.js
@@ -0,0 +1,14 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-DeHZSEgm.js";import{n,t as r}from"./Button-BXzGpppg.js";var i,a,o,s,c,l;function u(){return(u=e((()=>{n(),i=t(),a={title:`ui/Button`,component:r,decorators:[e=>(0,i.jsx)(`div`,{style:{width:`200px`,padding:`10px`},children:(0,i.jsx)(e,{})})]},o={args:{children:`Save`}},s={args:{children:`Save the current scene to scene.json`}},c={args:{children:`Submit`,type:`submit`}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Save'
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Save the current scene to scene.json'
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Submit',
+    type: 'submit'
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`,`LongLabel`,`Submit`]})))()}u();export{o as Default,s as LongLabel,c as Submit,l as __namedExportsOrder,a as default};
