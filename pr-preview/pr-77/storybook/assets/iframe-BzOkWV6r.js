8107395ac5c86de355bb91555eaf26c984bcb651
ukonpower <ukonpower@users.noreply.github.com>
2026-09-22 11:47:15 +0000

    Deploy preview for PR 77 🛫

--- pr-preview/pr-77/storybook/assets/iframe-BzOkWV6r.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./iframe-CaV60C46.js";e();
---
+++ pr-preview/pr-77/storybook/assets/index.stories-CrBzyK5U.js
@@ -0,0 +1,8 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{c as t,h as n,i as r,m as i,s as a,t as o}from"./scene-WhzRbgD4.js";import{t as s}from"./jsx-runtime-DeHZSEgm.js";import{t as c}from"./uipower-BQ76F1tE.js";import{t as l}from"./Panel-h7tqFVYO.js";var u,d,f,p,m,h,g;function _(){return(_=e((()=>{a(),o(),c(),n(),u=s(),d={title:`OREditor/SceneControl`,component:i},f=e=>[e=>(0,u.jsx)(`div`,{style:{width:`300px`,height:`240px`},children:(0,u.jsx)(l,{children:(0,u.jsx)(e,{})})}),t(e)],p={names:[`main`,`test`],current:`main`,onSelect:()=>{},onCreate:()=>{},onDelete:()=>{}},m={decorators:f({...r,scenes:p})},h={decorators:f(r)},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  decorators: decorators({
+    ...storyFixture,
+    scenes
+  })
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  decorators: decorators(storyFixture)
+}`,...h.parameters?.docs?.source}}},g=[`Default`,`NoSceneSelection`]})))()}_();export{m as Default,h as NoSceneSelection,g as __namedExportsOrder,d as default};
