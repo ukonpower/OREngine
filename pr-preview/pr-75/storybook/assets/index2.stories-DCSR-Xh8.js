34c982c70097e9a3ded39389fa552743a984e130
ukonpower <ukonpower@users.noreply.github.com>
2026-09-22 09:28:36 +0000

    Deploy preview for PR 75 🛫

---
+++ pr-preview/pr-75/storybook/assets/index2.stories-DCSR-Xh8.js
@@ -0,0 +1,16 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{r as t}from"./iframe-CZZYXPP7.js";import{t as n}from"./jsx-runtime-DeHZSEgm.js";import{f as r,o as i,p as a,s as o}from"./uipower-Ao9Ub4jC.js";import{n as s,t as c}from"./withInputWindow-C9WKbIyI.js";var l,u,d,f,p,m,h,g,_,v;function y(){return(y=e((()=>{l=t(),c(),r(),o(),u=n(),d={title:`ui/InputWindow`,component:i},f=()=>void 0,p=({config:e})=>{let{open:t}=a();return(0,l.useEffect)(()=>{t(e)},[t,e]),null},m=e=>({render:()=>(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(p,{config:e}),(0,u.jsx)(i,{})]}),decorators:[s]}),h=m({type:`number`,value:1.5,label:`Position X`,step:.1,onChange:f}),g=m({type:`text`,value:`OREngineCube`,label:`Entity Name`,onChange:f}),_=m({type:`number`,value:0,onChange:f}),h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`pattern({
+  type: 'number',
+  value: 1.5,
+  label: 'Position X',
+  step: 0.1,
+  onChange
+})`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`pattern({
+  type: 'text',
+  value: 'OREngineCube',
+  label: 'Entity Name',
+  onChange
+})`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`pattern({
+  type: 'number',
+  value: 0,
+  onChange
+})`,..._.parameters?.docs?.source}}},v=[`NumberValue`,`TextValue`,`NoLabel`]})))()}y();export{_ as NoLabel,h as NumberValue,g as TextValue,v as __namedExportsOrder,d as default};
