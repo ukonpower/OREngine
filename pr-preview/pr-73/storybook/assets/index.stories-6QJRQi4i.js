ed6550a9225528ffe5fb869cae459a08825cb4a8
ukonpower <ukonpower@users.noreply.github.com>
2026-09-22 08:53:48 +0000

    Remove preview for PR 73 🛬

--- pr-preview/pr-73/storybook/assets/index.stories-6QJRQi4i.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{c as t,h as n,i as r,m as i,s as a,t as o}from"./scene-B0L9lwiA.js";import{t as s}from"./jsx-runtime-DeHZSEgm.js";import{t as c}from"./uipower-Df2rZ3R5.js";import{t as l}from"./Panel-h7tqFVYO.js";var u,d,f,p,m,h,g;function _(){return(_=e((()=>{a(),o(),c(),n(),u=s(),d={title:`OREditor/SceneControl`,component:i},f=e=>[e=>(0,u.jsx)(`div`,{style:{width:`300px`,height:`240px`},children:(0,u.jsx)(l,{children:(0,u.jsx)(e,{})})}),t(e)],p={names:[`main`,`test`],current:`main`,onSelect:()=>{},onCreate:()=>{},onDelete:()=>{}},m={decorators:f({...r,scenes:p})},h={decorators:f(r)},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
-  decorators: decorators({
-    ...storyFixture,
-    scenes
-  })
-}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
-  decorators: decorators(storyFixture)
-}`,...h.parameters?.docs?.source}}},g=[`Default`,`NoSceneSelection`]})))()}_();export{m as Default,h as NoSceneSelection,g as __namedExportsOrder,d as default};
