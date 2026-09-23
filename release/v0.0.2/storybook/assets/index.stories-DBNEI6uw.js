6c3422b71b24488c72c0fa9964e051433e9077c5
ukonpower <ore.ukonpower@gmail.com>
2026-09-23 12:10:06 +0000

    Deploying to gh-pages from @ ukonpower/OREngine@c303d53ae85ff47e01185102278a404f3a507e03 🚀

--- release/v0.0.2/storybook/assets/index.stories-DBNEI6uw.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./SceneWindow-COWOvGID.js";var r,i,a,o;function s(){return(s=e((()=>{t(),r={title:`OREditor/SceneWindow`,component:n},i={names:[`main`,`intro`,`title`,`test`],current:`main`,onSelect:()=>{},onCreate:()=>{},onDelete:()=>{}},a={args:{scenes:i,projectName:`demo-webgl`,onClose:()=>{}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    scenes,
-    projectName: 'demo-webgl',
-    onClose: () => {/* storybook では閉じない */}
-  }
-}`,...a.parameters?.docs?.source}}},o=[`Default`]})))()}s();export{a as Default,o as __namedExportsOrder,r as default};
