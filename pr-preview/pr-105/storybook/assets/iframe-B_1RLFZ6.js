db3b60eecf2047521d723d207f52b3f0d0ecaaa7
ukonpower <ukonpower@users.noreply.github.com>
2026-09-23 10:36:49 +0000

    Deploy preview for PR 105 🛫

---
+++ pr-preview/pr-105/storybook/assets/iframe-B_1RLFZ6.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-Cc5mFMPq.js";e();
---
+++ pr-preview/pr-105/storybook/assets/index.stories-C72G6v1P.js
@@ -0,0 +1,3 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{A as t,c as n,i as r,j as i,s as a,t as o}from"./scene-hRA5Jg81.js";import{t as s}from"./jsx-runtime-DeHZSEgm.js";import{t as c}from"./uipower-DPeqnsWT.js";import{t as l}from"./Panel-h7tqFVYO.js";var u,d,f,p,m;function h(){return(h=e((()=>{a(),o(),c(),i(),u=s(),d={title:`OREditor/EditorSettings`,component:t},f=[e=>(0,u.jsx)(`div`,{style:{width:`300px`,height:`240px`},children:(0,u.jsx)(l,{children:(0,u.jsx)(e,{})})}),n(r)],p={decorators:f},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  decorators
+}`,...p.parameters?.docs?.source}}},m=[`Default`]})))()}h();export{p as Default,m as __namedExportsOrder,d as default};
