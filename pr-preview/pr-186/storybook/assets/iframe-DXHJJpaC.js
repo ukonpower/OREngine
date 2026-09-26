d1619d38ea04aea4047667b77f523a8ea464d74d
ukonpower <ukonpower@users.noreply.github.com>
2026-09-26 17:23:09 +0000

    Deploy preview for PR 186 🛫

---
+++ pr-preview/pr-186/storybook/assets/iframe-DXHJJpaC.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-s-Q5gSHK.js";e();
---
+++ pr-preview/pr-186/storybook/assets/index.stories-BhF5QhPA.js
@@ -0,0 +1,36 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{r as t}from"./iframe-s-Q5gSHK.js";import{t as n}from"./jsx-runtime-DeHZSEgm.js";import{_ as r,g as i}from"./uipower-BzvQBvqX.js";import{n as a,t as o}from"./withInputWindow-DByj6ukA.js";var s,c,l,u,d,f,p,m,h,g,_,v;function y(){return(y=e((()=>{s=t(),o(),r(),c=n(),l=e=>{let[t,n]=(0,s.useState)(e.value);return(0,c.jsx)(i,{...e,value:t,onChange:n})},u={title:`ui/Input/InputNumber`,component:i,args:{value:1.234},render:e=>(0,c.jsx)(l,{...e}),decorators:[e=>(0,c.jsx)(`div`,{style:{width:`160px`,padding:`10px`},children:(0,c.jsx)(e,{})}),a]},d={},f={args:{value:.5,step:.1,min:0,max:1}},p={args:{value:8,step:1,min:1,max:64,int:!0}},m={args:{value:3.14159265,precision:5}},h={args:{value:42,selected:!0}},g={args:{value:42,readOnly:!0}},_={args:{value:42,disabled:!0}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 0.5,
+    step: 0.1,
+    min: 0,
+    max: 1
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 8,
+    step: 1,
+    min: 1,
+    max: 64,
+    int: true
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 3.14159265,
+    precision: 5
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 42,
+    selected: true
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 42,
+    readOnly: true
+  }
+}`,...g.parameters?.docs?.source}}},_.parameters={..._.parameters,docs:{..._.parameters?.docs,source:{originalSource:`{
+  args: {
+    value: 42,
+    disabled: true
+  }
+}`,..._.parameters?.docs?.source}}},v=[`Default`,`WithRange`,`Int`,`Precision`,`Selected`,`ReadOnly`,`Disabled`]})))()}y();export{d as Default,_ as Disabled,p as Int,m as Precision,g as ReadOnly,h as Selected,f as WithRange,v as __namedExportsOrder,u as default};
