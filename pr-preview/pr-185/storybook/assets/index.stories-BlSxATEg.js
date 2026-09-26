6f452ea58b9f628814afb953b7d06c8da8366aaa
ukonpower <ukonpower@users.noreply.github.com>
2026-09-26 17:05:45 +0000

    Deploy preview for PR 185 🛫

---
+++ pr-preview/pr-185/storybook/assets/index.stories-BlSxATEg.js
@@ -0,0 +1,32 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./ArrowIcon-DTg42cjh.js";import{t as n}from"./jsx-runtime-DeHZSEgm.js";import{n as r,t as i}from"./Button-C0LgJv2v.js";import{t as a}from"./Icons-BugE91nR.js";var o,s,c,l,u,d,f,p,m;function h(){return(h=e((()=>{a(),r(),o=n(),s={title:`ui/Button`,component:i,decorators:[e=>(0,o.jsx)(`div`,{style:{width:`200px`,padding:`10px`},children:(0,o.jsx)(e,{})})]},c={args:{children:`Save`}},l={args:{children:`Save the current scene to scene.json`}},u={args:{children:`Submit`,type:`submit`}},d={args:{children:`Save`},decorators:[e=>(0,o.jsx)(`div`,{style:{display:`grid`},children:(0,o.jsx)(e,{})})]},f={args:{children:`Order`,active:!0}},p={args:{children:(0,o.jsx)(t,{direction:`left`}),square:!0,title:`Previous`}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Save'
+  }
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Save the current scene to scene.json'
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Submit',
+    type: 'submit'
+  }
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Save'
+  },
+  decorators: [Story => <div style={{
+    display: 'grid'
+  }}><Story /></div>]
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: 'Order',
+    active: true
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: <ArrowIcon direction="left" />,
+    square: true,
+    title: 'Previous'
+  }
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`LongLabel`,`Submit`,`FullWidth`,`Active`,`Square`]})))()}h();export{f as Active,c as Default,d as FullWidth,l as LongLabel,p as Square,u as Submit,m as __namedExportsOrder,s as default};
